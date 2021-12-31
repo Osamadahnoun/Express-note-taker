9adb63d1ac4d91345034663bdccc39642c6a565a
Osama Dahnoun <omdahnou@ncsu.edu>
2021-12-30 19:11:05 -0500

    can add notes to json on the backend

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
+app.use(express.static('public'));
 
 
 const notes = require('./db/db.json');
 // const indexHTML = require('./public/index.html');
 // const notesHTML = require('./public/notes.html');
+
+const createNewNote = function(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+      );
+    return note;
+};
 
 app.get('/api/notes', (req, res) => {
     res.json(notes)
@@ -19,6 +35,14 @@ app.get('/notes', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+app.post('/api/notes', (req , res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes)
+
+    res.json(note);
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`)
